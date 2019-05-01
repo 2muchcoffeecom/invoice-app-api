ef67b47a375a10fd169f578b72de72517c14bf09
Anton Rashchepkin <anton.r@2muchcoffee.com>
2019-05-01 18:06:04 +0300

    refactor(app): changed graphQl config to service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GraphQLModule } from '@nestjs/graphql';
 
-import * as _ from 'lodash';
-
 import { SharedModule } from './modules/shared/shared.module';
 import { CoreModule } from './modules/core/core.module';
+import { GqlConfigService } from './modules/core/services/gql-config.service';
 import { CustomerModule } from './modules/customer/customer.module';
 import { ProductModule } from './modules/product/product.module';
 import { InvoiceModule } from './modules/invoice/invoice.module';
@@ -21,40 +20,9 @@ import { ScalarModule } from './modules/shared/modules/scalar/scalar.module';
         uri: config.get('MONGODB_URI'),
       }),
     }),
-    GraphQLModule.forRoot({
-      playground: true,
-      typePaths: ['./**/*.graphql'],
-      context: ({req, res}) => ({req, res}),
-      formatError: (err) => {
-        console.log('ERROR', err);
-
-        let data: any;
-        let message: string;
-        let code: string;
-        let fields: string;
-
-        try {
-          data = JSON.parse(err.message);
-        } catch (e) {
-          data = err.message;
-        }
-
-        if (_.isString(data) || _.isInteger(data)) {
-          message = data;
-        }
-        if (_.isObject(data)) {
-          fields = data.fields || undefined;
-          code = data.code || undefined;
-          message = data.message || undefined;
-        }
-        return {
-          message,
-          fields,
-          code: err.originalError && err.originalError.code || code,
-          locations: err.locations,
-          path: err.path,
-        };
-      },
+    GraphQLModule.forRootAsync({
+      inject: [GqlConfigService],
+      useExisting: GqlConfigService,
     }),
     SharedModule,
     CoreModule,
