009ef36392fd50ca0cfde9a21be2c687903f1a9a
Stanislav Mykhailenko <stas.m@2muchcoffee.com>
2019-05-06 18:49:32 +0300

    build(app) set express-typescript configuration

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { INestApplication, INestExpressApplication } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as cors from 'cors';
-import * as express from 'express';
+import errorHandler from 'errorhandler';
 
-import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './modules/shared/errors/http-exception.filter';
-import { ExceptionInterceptor } from './modules/shared/errors/exception.interceptor';
-import { ErrorsService } from './modules/shared/services/errors.service';
+import app from './app';
 
+/**
+ * Error Handler. Provides full stack - remove for production
+ */
+app.use(errorHandler());
 
-export let app: INestApplication & INestExpressApplication;
+/**
+ * Start Express main.
+ */
+const main = app.listen(app.get('port'), () => {
+  console.log('App is running at http://localhost:%d', app.get('port'));
+});
 
-async function bootstrap() {
-  const server = express();
-  server.use(cors());
-
-  app = await NestFactory.create(AppModule, server);
-
-  const options = new DocumentBuilder()
-    .setTitle('Invoice App Swagger')
-    .setDescription('API for Invoice App')
-    .setVersion('1.0')
-    .setBasePath('api')
-    .setSchemes('https')
-    .addTag('Customer')
-    .addTag('Product')
-    .addTag('Invoice')
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('/swagger', app, document);
-  app.useGlobalInterceptors(new ExceptionInterceptor(new ErrorsService()));
-  app.useGlobalFilters(new HttpExceptionFilter());
-  app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT || 3000);
-}
-
-bootstrap();
+export default main;
