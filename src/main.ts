61fb9738e868e595542abda65c4919b0a78c6488
Anton Rashchepkin <anton.r@2muchcoffee.com>
2019-05-07 15:23:29 +0300

    feat(main.ts): changed global providers

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './modules/shared/errors/http-exception.filter';
-import { ExceptionInterceptor } from './modules/shared/errors/exception.interceptor';
-import { ErrorsService } from './modules/core/services/errors.service';
 import { ConfigService } from './modules/core/services/config.service';
+import { ParseErrorInterceptor } from './modules/shared/errors/parse-error.interceptor';
 
 
 export let app: INestApplication;
@@ -14,6 +13,9 @@ export let app: INestApplication;
 async function bootstrap() {
   app = await NestFactory.create(AppModule);
 
+  /**
+   * Swagger Setup
+   */
   const options = new DocumentBuilder()
     .setTitle('Invoice App Swagger')
     .setDescription('API for Invoice App')
@@ -26,8 +28,18 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/swagger', app, document);
-  app.useGlobalInterceptors(new ExceptionInterceptor(new ErrorsService()));
+  // app.useGlobalInterceptors(new ExceptionInterceptor(new ErrorsService()));
+  /**
+   * Used for handling error and transforming them to parsed error
+   * Used mostly for resolvers
+   */
+  app.useGlobalInterceptors(new ParseErrorInterceptor());
+
+  /**
+   * Used for HttpExeptions that are thrown by Rest API controllers
+   */
   app.useGlobalFilters(new HttpExceptionFilter());
+
   app.setGlobalPrefix('api');
   app.enableCors();
   const port = app.get(ConfigService).get('PORT');
