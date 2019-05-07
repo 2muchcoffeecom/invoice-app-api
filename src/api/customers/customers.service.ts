c8a07b62db53e37ddda2e7cc05d0559eabab01c2
Stanislav Mykhailenko <stas.m@2muchcoffee.com>
2019-05-07 12:41:49 +0300

    feat(api:customers) create function to get customer

--- src/api/customers/customers.service.ts
+++ src/api/customers/customers.service.ts
@@ -1,7 +1,16 @@
 import { DocumentQuery } from "mongoose";
+
 import { ICustomer } from './customer.interface';
 import Customer from './customer.model'
 
 export function getCustomersFromDb(): DocumentQuery<ICustomer[], ICustomer> {
   return Customer.find({});
+}
+
+export async function getCustomerFromDb(id: string): Promise<ICustomer>  {
+  const customer = await Customer.findById(id);
+  if (!customer) {
+    throw Error('Customer not found');
+  }
+  return customer;
 }
