53d2ef78bb61a730a45194431b634cede1dc99b3
Stanislav Mykhailenko <stas.m@2muchcoffee.com>
2019-05-07 12:59:13 +0300

    feat(api:customers) create createCustomerInDb function

--- src/api/customers/customers.service.ts
+++ src/api/customers/customers.service.ts
@@ -7,6 +7,11 @@ export function getCustomersFromDb(): DocumentQuery<ICustomer[], ICustomer> {
   return Customer.find({});
 }
 
+export function createCustomerInDb(newCustomer: ICustomer): Promise<ICustomer> {
+  const newEntity = new Customer(newCustomer);
+  return newEntity.save();
+}
+
 export async function getCustomerFromDb(id: string): Promise<ICustomer>  {
   const customer = await Customer.findById(id);
   if (!customer) {
