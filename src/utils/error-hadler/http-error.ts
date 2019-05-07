eed923aab5d6f2ce5c8af0533a26f17fb5711279
Stanislav Mykhailenko <stas.m@2muchcoffee.com>
2019-05-07 16:03:35 +0300

    feat(utils:error-hadler) create HttpError class

---
+++ src/utils/error-hadler/http-error.ts
@@ -0,0 +1,10 @@
+export class HttpError extends Error {
+  statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+
+    this.name = this.constructor.name;
+    this.statusCode = statusCode;
+  }
+}
