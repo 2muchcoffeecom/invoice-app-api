4f0b911f63455eab0066a1c9b86a74d6f1389b4b
Stanislav Mykhailenko <stas.m@2muchcoffee.com>
2019-05-07 13:14:10 +0300

    feat(api:customers) add route to update customer

--- src/api/customers/index.ts
+++ src/api/customers/index.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
-import { createCustomer, getCustomer, getCustomers } from './customers.controller';
+import { createCustomer, getCustomer, getCustomers, updateCustomer } from './customers.controller';
 
 const router = Router();
 
 router.get('/', getCustomers);
 router.post('/', createCustomer);
 router.get('/:id', getCustomer);
+router.put('/:id', updateCustomer);
 
 export default router;
