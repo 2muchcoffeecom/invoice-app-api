61d8c03fece86120287820d2eb966b6188dd7968
Stanislav Mykhailenko <stas.m@2muchcoffee.com>
2019-05-07 12:07:56 +0300

    feat(app) add customers routs

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,10 @@
 import { Express, Request, Response } from 'express';
 
+import customersRouts from './api/customers';
+
 export default function(app: Express) {
+  app.use('/customers', customersRouts);
+
   // All undefined routes should return a 404
   app.route('/*').get((req: Request, res: Response) => {
     res.status(404).send('Page not found');
