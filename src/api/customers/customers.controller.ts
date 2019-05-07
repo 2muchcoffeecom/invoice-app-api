b9df39a8e09714f00383d338965ca2d1120ec281
Stanislav Mykhailenko <stas.m@2muchcoffee.com>
2019-05-07 12:59:44 +0300

    feat(api:customers) create createCustomer function

--- src/api/customers/customers.controller.ts
+++ src/api/customers/customers.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 
-import { getCustomerFromDb, getCustomersFromDb } from './customers.service';
+import { createCustomerInDb, getCustomerFromDb, getCustomersFromDb } from './customers.service';
 
 export function getCustomers(req: Request, res: Response): void {
   getCustomersFromDb()
@@ -9,6 +9,18 @@ export function getCustomers(req: Request, res: Response): void {
   })
   .catch(() => {
     res.status(500).json({ message: 'An error occurred while processing the request' });
+  });
+}
+
+export function createCustomer(req: Request, res: Response): void {
+  const newCustomer = req.body;
+
+  createCustomerInDb(newCustomer)
+  .then((newCustomer) => {
+    res.status(201).json(newCustomer)
+  })
+  .catch((err) => {
+    res.status(500).json({ message: err.message });
   });
 }
 
