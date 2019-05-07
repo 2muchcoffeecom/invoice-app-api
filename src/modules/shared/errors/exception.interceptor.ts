6f6a222bfa251a9339dc94ec529a3410e03d457b
Anton Rashchepkin <anton.r@2muchcoffee.com>
2019-05-07 15:16:19 +0300

    feat(shared:error): update exception interceptor to use parse error

--- src/modules/shared/errors/exception.interceptor.ts
+++ src/modules/shared/errors/exception.interceptor.ts
@@ -2,26 +2,28 @@ import { HttpException, Injectable, ExecutionContext, CallHandler, NestIntercept
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-import { ErrorsService } from '../../core/services/errors.service';
+import { ParsedError } from './parsed-error';
 
 
 @Injectable()
 export class ExceptionInterceptor implements NestInterceptor {
-  constructor(
-    private errorsService: ErrorsService,
-  ) {
-  }
 
   intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> {
     return next
       .handle()
       .pipe(
         catchError(error => {
-          const resError = this.errorsService.parse(error);
+          let resError = error;
+          /**
+           * Condition to parse error if unhandled before
+           */
+          if (!(error instanceof ParsedError)) {
+            resError = new ParsedError(error);
+          }
           return throwError(
             new HttpException(
               resError.message,
-              resError.status,
+              resError.code,
             ),
           );
         }),
