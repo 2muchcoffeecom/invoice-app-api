ce240619e40a5da1adf7efec1e896622d201d044
Stanislav Mykhailenko <stas.m@2muchcoffee.com>
2019-05-07 19:16:45 +0300

    feat(modules:customers) change customer schema

--- src/modules/customers/customer.model.ts
+++ src/modules/customers/customer.model.ts
@@ -5,12 +5,16 @@ import { ICustomer } from './customer.interface';
 const customerSchema = new Schema({
   name: {
     type: String,
+    trim: true,
     required: true,
     unique: true,
     uniqueCaseInsensitive: true,
   },
-  address: { type: String, required: true },
-  phone: { type: String, required: true },
+  address: { type: String, trim: true, required: true },
+  phone: { type: String, trim: true, required: true },
+}, {
+  versionKey: false,
+  timestamps: true,
 });
 
 const customer = model<ICustomer>('Customer', customerSchema);
