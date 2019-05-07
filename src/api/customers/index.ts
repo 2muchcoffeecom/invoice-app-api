e4a4dfe52934327c54751eca186e1abb920744ca
Stanislav Mykhailenko <stas.m@2muchcoffee.com>
2019-05-07 12:44:04 +0300

    feat(api:customers) add route to get customer

--- src/api/customers/index.ts
+++ src/api/customers/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
-import { getCustomers } from './customers.controller';
+import { getCustomer, getCustomers } from './customers.controller';
 
 const router = Router();
 
 router.get('/', getCustomers);
+router.get('/:id', getCustomer);
 
 export default router;
