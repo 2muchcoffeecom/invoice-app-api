aa75d0d9769a6fed3a69ae3f0d695089ddf8de8a
Stanislav Mykhailenko <stas.m@2muchcoffee.com>
2019-05-09 14:41:10 +0300

    feat(modules:invoices) create countInvoiceTotal function

--- src/modules/invoices/invoices.service.ts
+++ src/modules/invoices/invoices.service.ts
@@ -1,4 +1,8 @@
 import { DocumentQuery } from 'mongoose';
+import { find as _find, map as _map, reduce as _reduce } from 'lodash';
+
+import { getProductsForInvoice } from '../products/products.service';
+import { getInvoiceItemsFromDb } from './invoice-items/invoice-items.service';
 
 import { IInvoice } from './invoice.interface';
 import Invoice from './invoice.schema';
@@ -41,4 +45,20 @@ export async function deleteInvoiceFromDb(id: string): Promise<IInvoice> {
     throw new HttpError('Invoice not found', 404);
   }
   return foundEntity.remove();
+}
+
+export async function countInvoiceTotal(invoiceId: string, discount: number): Promise<number> {
+  const items = await getInvoiceItemsFromDb(invoiceId);
+
+  const productIds = _map(items, item => item.product_id);
+
+  const products = await getProductsForInvoice(productIds);
+
+  const totalWithoutDiscount = _reduce(items, (total, item) => {
+    const product = _find(products, ['_id', item.product_id]);
+    const itemTotal = item.quantity * product.price;
+    return total + itemTotal;
+  }, 0);
+
+  return totalWithoutDiscount * (1 - discount/100);
 }
