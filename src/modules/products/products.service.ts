5c7a52ba7e92355f075570ad7fe7ce05d4284383
Stanislav Mykhailenko <stas.m@2muchcoffee.com>
2019-05-09 14:40:12 +0300

    feat(modules:products) create getProductsForInvoice function

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -42,3 +42,7 @@ export async function deleteProductFromDb(id: string): Promise<IProduct> {
   }
   return deletedEntity;
 }
+
+export function getProductsForInvoice(ids: string[]): DocumentQuery<IProduct[], IProduct> {
+  return Product.find({ _id: { $in: ids } })
+}
